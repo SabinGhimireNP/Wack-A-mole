1e3b67b79ffe1e7c682183335f985473e58c8e86
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-15 22:33:34 +0545

    working

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+const tryAgain = document.querySelector(".tryAgain");
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+const timerDisplay = document.querySelector(".timer");
+const Start = document.querySelector(".btn");
 let lastHole;
 let timeUp = false;
+let score = 0;
+let timerCount;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -26,14 +31,40 @@ function peep() {
   }, time);
 }
 
-function StartGame() {
+function StartGame(e) {
+  tryAgain.style.display = "none";
+
+  console.log(e);
+  holes.forEach((hole) => (hole.style.display = "block"));
+  Start.style.display = "none";
   scoreBoard.textContent = 0;
   timeUp = false;
+  score = 0;
   peep();
+  timer();
+  setTimeout(() => (timeUp = true), 10000);
 }
 
-function bonk() {
-  
+function bonk(e) {
+  if (!e.isTrusted) return; // remove fake clickss
+  score++;
+  this.classList.remove("up");
+  scoreBoard.textContent = score;
+}
+
+function timer() {
+  timerCount = 10;
+  const countdown = setInterval(() => {
+    timerCount--;
+    timerDisplay.textContent = ` ${
+      timerCount <= 0 ? "Game Over!" : `Game Over in : ${timerCount} seconds`
+    } `;
+    if (timerCount <= 0) {
+      clearInterval(countdown);
+      tryAgain.style.display = "block";
+      timeUp = true;
+    }
+  }, 1000);
 }
 
 moles.forEach((mole) => mole.addEventListener("click", bonk));
