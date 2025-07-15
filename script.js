764cba1bd17a04f56ce5dfb1be107db124157499
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-15 22:37:56 +0545

    polished

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+// Varaibles declartion
 const tryAgain = document.querySelector(".tryAgain");
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
 const timerDisplay = document.querySelector(".timer");
 const Start = document.querySelector(".btn");
+
 let lastHole;
 let timeUp = false;
 let score = 0;
 let timerCount;
 
+//Function declaration
+
+//Decides the time mole is shown
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
+// makes sure that there is a random hole every time
 function randomHole(holes) {
   const idx = Math.floor(Math.random() * holes.length);
   const hole = holes[idx];
@@ -21,6 +27,7 @@ function randomHole(holes) {
   return hole;
 }
 
+//decides where the mole will be seen
 function peep() {
   const time = randomTime(200, 1000);
   const hole = randomHole(holes);
@@ -31,10 +38,9 @@ function peep() {
   }, time);
 }
 
+// Starts the game
 function StartGame(e) {
   tryAgain.style.display = "none";
-
-  console.log(e);
   holes.forEach((hole) => (hole.style.display = "block"));
   Start.style.display = "none";
   scoreBoard.textContent = 0;
@@ -45,6 +51,7 @@ function StartGame(e) {
   setTimeout(() => (timeUp = true), 10000);
 }
 
+// Handels the click and scoreBoard
 function bonk(e) {
   if (!e.isTrusted) return; // remove fake clickss
   score++;
@@ -52,6 +59,7 @@ function bonk(e) {
   scoreBoard.textContent = score;
 }
 
+// Manages the timer
 function timer() {
   timerCount = 10;
   const countdown = setInterval(() => {
@@ -67,4 +75,5 @@ function timer() {
   }, 1000);
 }
 
+// AddEventListner
 moles.forEach((mole) => mole.addEventListener("click", bonk));
